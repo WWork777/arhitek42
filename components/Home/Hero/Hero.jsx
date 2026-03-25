508b6b2eb7a7b5b4893376cb5290777ae1ada8e3
Ser0jka123 <sersur42@yandex.ru>
2026-03-25 09:15:09 +0700

    upd

--- components/Home/Hero/Hero.jsx
+++ components/Home/Hero/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero({
   backgroundSrc = "/Home/video.mp4",
   title = "Строительство домов под ключ",
   description = "ОПЫТ БОЛЕЕ 25 ЛЕТ",
-  buttonName = "Узнать стоимость",
+  buttonName = "Узнать стоимость строительства",
   buttonType = "default"
 }) {
   const backgroundType = useMemo(() => {
@@ -18,12 +18,14 @@ export default function Hero({
   const buttonAttributes = buttonType === "default" 
     ? { 
         "data-bs-toggle": "modal", 
-        "data-bs-target": "#exampleModal" 
+        "data-bs-target": "#exampleModa2",
+        "data-bs-title": buttonName
       }
     : { 
         "data-bs-toggle": "modal", 
-        // "data-bs-target": "#exampleModa2" 
-        "data-bs-target": "#exampleModal" 
+        "data-bs-target": "#exampleModa2",
+        "data-bs-title": buttonName
+        // "data-bs-target": "#exampleModal"
       };
 
   return (
@@ -51,7 +53,7 @@ export default function Hero({
             className={`btn ${buttonType === "default" ? "primary-btn" : "secondary-btn"}`}
             {...buttonAttributes}
           >
-            {buttonName}
+            Узнать стоимость
           </button>
         </div>
       </div>
