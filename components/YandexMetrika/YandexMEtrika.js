12f5b440b377ab0295070164ae3b11c7b1788d0b
id0ntc4re <parsikovevgenij470@gmail.com>
2026-05-14 14:59:21 +0700

    qwe

--- components/YandexMetrika/YandexMEtrika.js
+++ components/YandexMetrika/YandexMEtrika.js
@@ -1,19 +1,26 @@
-"use client"
-import { useEffect } from 'react';
+"use client";
+import { useEffect } from "react";
 
 export default function YandexMetrika() {
   useEffect(() => {
-    (function(m, e, t, r, i, k, a) {
-      m[i] = m[i] || function() { (m[i].a = m[i].a || []).push(arguments); };
+    (function (m, e, t, r, i, k, a) {
+      m[i] =
+        m[i] ||
+        function () {
+          (m[i].a = m[i].a || []).push(arguments);
+        };
       m[i].l = 1 * new Date();
       for (let j = 0; j < document.scripts.length; j++) {
         if (document.scripts[j].src === r) return;
       }
-      k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r;
+      ((k = e.createElement(t)),
+        (a = e.getElementsByTagName(t)[0]),
+        (k.async = 1),
+        (k.src = r));
       a.parentNode.insertBefore(k, a);
-    })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-    ym(98007197, 'init', {
+    ym(109198237, "init", {
       clickmap: true,
       trackLinks: true,
       accurateTrackBounce: true,
@@ -25,8 +32,8 @@ export default function YandexMetrika() {
     <noscript>
       <div>
         <img
-          src="https://mc.yandex.ru/watch/98007197"
-          style={{ position: 'absolute', left: '-9999px' }}
+          src="https://mc.yandex.ru/watch/109198237"
+          style={{ position: "absolute", left: "-9999px" }}
           alt=""
         />
       </div>
